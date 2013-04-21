ceda14e50cd4bdedfbb15e81dbfa0859bb6a086b
Axel Magnuson <axelmagn@gmail.com>
2013-04-21 17:54:13 -0600

    start writing custom push api endpoint

--- snippets/api.py
+++ snippets/api.py
@@ -1,7 +1,11 @@
-from rest_framework import generics, views
+from rest_framework import generics, views, mixins
+from rest_framework.response import Response
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
+import pdb # DEBUG
 
+# session key for currently active snippet
+SESSION_SNIPPET_ID = 'snippet_id'
 
 
 class SnippetCreate(generics.CreateAPIView):
@@ -26,18 +30,31 @@ class SnippetRetrieve(generics.RetrieveAPIView):
 
 
 
-class SnippetPush(views.APIView):
+class SnippetPush(generics.GenericAPIView,
+                  mixins.CreateModelMixin,
+                  mixins.UpdateModelMixin):
     """
     Session-based Snipped posting endpoint
 
     Create/Update functionality, using sessions to resolve snippet id.
 
     """
-    def post(self, request, format=None):
-        # make sure id isn't already specified
-        # figure out if we're creating or updating
-        # if creating, call post from SnippetCreate
-        # if updating
-            # populate snippet id w/ cached session info
-            # call post from SnippetUpdate
+    model = Snippet
+    serializer_class = SnippetSerializer
 
+    def post(self, request, *args, **kwargs):
+        # figure out if we're creating or updating
+        snippet_id = request.session.get(SESSION_SNIPPET_ID)
+        snippet_is_new = snippet_id == None
+        # if creating, call post from SnippetCreate
+        if snippet_is_new:
+            response = self.create(request, *args, **kwargs)
+            request.session[SESSION_SNIPPET_ID] = response.data['id']
+            return response
+        # if updating
+        else:
+            # populate snippet id w/ cached session info
+            # TODO: request.DATA is immutable.  Must create copy. BUG
+            request.DATA['id'] = snippet_id
+            # call post from SnippetUpdate
+            return self.update(self, request, *args, **kwargs)
--- snippets/urls.py
+++ snippets/urls.py
@@ -5,6 +5,7 @@ from snippets import api
 urlpatterns = patterns('',
     url(r'^api/snippets/create/$', api.SnippetCreate.as_view()),
     url(r'^api/snippets/(?P<pk>[0-9]+)/$', api.SnippetRetrieve.as_view()),
+    url(r'^api/snippets/push/$', api.SnippetPush.as_view()),
 )
 
 urlpatterns = format_suffix_patterns(urlpatterns)
