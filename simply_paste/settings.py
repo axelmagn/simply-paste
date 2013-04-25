fd457a3c7abd7958b1ccf0c3d8819d73f22ef06b
Axel Magnuson <axelmagn@gmail.com>
2013-04-24 21:11:04 -0600

    hack away at django-require

--- simply_paste/settings.py
+++ simply_paste/settings.py
@@ -207,4 +207,4 @@ REQUIRE_EXCLUDE = ("build.txt",)
 # The execution environment in which to run r.js: auto, node or rhino.
 # auto will autodetect the environment and make use of node if available and rhino if not.
 # It can also be a path to a custom class that subclasses require.environments.Environment and defines some "args" function that returns a list with the command arguments to execute.
-REQUIRE_ENVIRONMENT = "auto"
+REQUIRE_ENVIRONMENT = "node"
