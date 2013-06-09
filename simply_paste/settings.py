75ae4422f0aadc8b31e2b795255e4450078160c4
Axel Magnuson <axelmagn@gmail.com>
2013-06-09 17:59:56 -0600

    disable debug in master branch

--- simply_paste/settings.py
+++ simply_paste/settings.py
@@ -9,7 +9,7 @@ import dj_database_url
 gettext = lambda s: s
 PROJECT_PATH = os.path.abspath(os.path.dirname(__file__))
 
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 STATIC_DEBUG = DEBUG
 REQUIRE_DEBUG = DEBUG
